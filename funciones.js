710de560b60c4c883e0fe2f4f30cfc60ef837a57
Marcos Alonso <marcosalonso@MBP-de-Marcos.home>
2021-06-03 22:27:02 +0200

    Añadiendo lineas

--- funciones.js
+++ funciones.js
@@ -1,3 +1,8 @@
-for (let i = 0; i < 100, i++) {
-    console.log("Soy javascript")
-}
+let elBoton = document.querySelector("#btn-aceptar") // Con esto capturamos el boton que está en el fichero index.html (el Document del DO)
+    .addEventListener('click', () => { // Add event listener es el capturador de eventos
+        laEntrada = document.querySelector('#entrada')
+        parrafo =document.createElement("p")
+        elfolio = document.querySelector("#folio")
+        elfolio.appendChild(parrafo)
+        parrafo.innerHTML = laEntrada.value
+    })
